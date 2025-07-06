73231a3cd0f1fdc1661cc54f2643d74a86c10b67
Van-Hoang-Dev <vanhoang24edu@gmail.com>
2025-07-06 17:42:10 +0700

    make CourseApiController, programs ui, student ui

---
+++ app/Http/Controllers/CourseApiController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Statamic\Facades\Entry;
+use Statamic\Query\EntryQueryBuilder;
+
+class CourseApiController extends Controller
+{
+    public function index()
+    {
+
+        /** @var EntryQueryBuilder $query $programs */
+        $query = Entry::query();
+
+        $courses = $query
+            ->where('collection', 'courses')
+            ->get()
+            ->map(function ($course) {
+                /** @var EntryQueryBuilder $query */
+                $query = Entry::query();
+                $programs = $query
+                 ->where('collection', 'programs')
+                 ->where('course', $course->id)
+                 ->get()
+                 ->map(function($program) {
+                    return [
+                        'title' => $program->title,
+                        'slug' => $program->slug,
+                        'url' => $program->url,
+                        'index' => $program->index,
+                    ];
+                 });
+
+                return [
+                    'title' => $course->title,
+                    'slug' => $course->slug(),
+                    'year' => $course->get('year'),
+                    'programs' => $programs->sortBy('index')->values(),
+                ];
+            })->sortByDesc('year')->values();
+
+        return response()->json($courses);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\CourseApiController;
 use Illuminate\Support\Facades\Route;
 
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+// API
+Route::get('/api/courses', [CourseApiController::class, 'index']);
+
+
+// ROUTE
+Route::statamic('/dao-tao/{course}/{program}', function (){
+    return view ('programs.show');
+} );
